0165e25a37ba25dc73fa4d3f576f640314040532
KimInteger <kjs20151240@gmail.com>
2024-06-12 10:53:07 +0900

    setter와 getter

--- work-1.js
+++ work-1.js
@@ -6,6 +6,44 @@ const basicData = (name, age = 0, job = '백수') => {
   }
 }
 
+class BasicData {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+
+  set name(value) {
+    if(typeof(value) === 'string'){
+      this._name = value;
+    } 
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(value) {
+    if(typeof(value) === 'number'){
+      this._age = value;
+    }
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set job(value) {
+    if(typeof(value) === 'string') {
+      this._job = value;
+    }
+  }
+
+  get job() {
+    return this._job
+  }
+}
+
 const component = (data={}) => {
   let obj = data;
 
@@ -27,5 +65,7 @@ const component = (data={}) => {
 
 };
 
-let a = component(basicData('김정수'));
+let a = component(new BasicData('김정수','29', 20));
 console.log(a);
+
+console.log(component(new BasicData('김인티저',29,'정수')));

236e6bcb617bbf9aa778c3e2950318d53ef0ce52
KimInteger <kjs20151240@gmail.com>
2024-06-12 11:52:16 +0900

    innerFunc 테스트

--- work-2.js
+++ work-2.js
@@ -1,1 +1,22 @@
-console.log('work-2.js')
+const component = () => {
+  let obj = {
+    name : "인티저 김",
+    age : 20,
+    job: "開発者"
+  };
+
+  let innerFunc = () => {
+    if(obj.age > 20) {
+      return '여름엔 역시 맥주가 최고지!';
+    } else {
+      return ("やっぱ、夏には牛乳が最高！");
+    }
+  }
+
+  return `
+  <h1>${obj.name}</h1>
+  <h2>${innerFunc()}</h2>
+  `;
+}
+
+console.log(component());

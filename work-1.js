b4f656ec0d912bf79c7e9ea2b4810af3f133bde2
KimInteger <kjs20151240@gmail.com>
2024-06-12 10:44:56 +0900

    객체를 함수의 리턴값으로 할당

--- work-1.js
+++ work-1.js
@@ -1,1 +1,31 @@
-console.log('work-1');
+const basicData = (name, age = 0, job = '백수') => {
+  return {
+    name : name,
+    age : age,
+    job : job
+  }
+}
+
+const component = (data={}) => {
+  let obj = data;
+
+  const decision = () => {
+
+  };
+
+  return `
+  <!DOCTYPE html>
+  <html lang="ko">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width initial-scale=1.0">
+      <title>${obj.name}</title>
+    </head>
+    <body>${obj.age} ${obj.job}</body>
+  </html>
+  `;
+
+};
+
+let a = component(basicData('김정수'));
+console.log(a);

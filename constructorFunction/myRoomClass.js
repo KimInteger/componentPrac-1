5f3fce24b8dd3bbbd5f7ca56a9c81ba0c2307d60
KimInteger <kjs20151240@gmail.com>
2024-06-13 10:52:29 +0900

    생성자 함수 초기화 및 가구를 변수에 할당

--- constructorFunction/myRoomClass.js
+++ constructorFunction/myRoomClass.js
@@ -1,1 +1,11 @@
-console.log('aaa');
+class myRoom {
+  constructor(name,group,type={}){
+    this.name = name;
+    this.group = group;
+    this.type = type;
+  }
+}
+
+let myFurniture = new myRoom('내방의 물건', '가구', {'의자' : '유어스', "책상":"보르네오","책장":"보르네오","침대":"없음","매트리스":"보르네오"});
+
+console.dir(myFurniture);

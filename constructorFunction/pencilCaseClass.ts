876258ed295b885246cac034fe89176ca654ba5b
KimInteger <kjs20151240@gmail.com>
2024-06-13 11:44:54 +0900

    string[]는 배열로 선언하는 것이 아니다. []로 배열임을 명시해줌으로써 push를 쓸 수 있었다.

--- constructorFunction/pencilCaseClass.ts
+++ constructorFunction/pencilCaseClass.ts
@@ -6,10 +6,16 @@ class PencilCase {
 
   printPencilCase = () : void => {
     let pencilCaseContent : string[];
-    this.type.forEach((item)=>{
+    pencilCaseContent = [];
+
+    this.type.forEach((item:string)=>{
       pencilCaseContent.push(item);
     });
     
     console.log(`나의 필통에는 ${this.type}이 들어있습니다.`);
   }
 }
+
+const pencilCase = new PencilCase(["샤프","볼펜","지우개","샤프심","연필","만년필"]);
+
+pencilCase.printPencilCase();

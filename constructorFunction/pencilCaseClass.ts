d45145d9280ed6d213874ee40b8a2ddbc13cc812
KimInteger <kjs20151240@gmail.com>
2024-06-13 11:40:09 +0900

    타스로 배열매서드 사용해보기

--- constructorFunction/pencilCaseClass.ts
+++ constructorFunction/pencilCaseClass.ts
@@ -1,1 +1,15 @@
-console.log('create');
+class PencilCase {
+  type : string[];
+  constructor( what : string[]) {
+    this.type = what;
+  }
+
+  printPencilCase = () : void => {
+    let pencilCaseContent : string[];
+    this.type.forEach((item)=>{
+      pencilCaseContent.push(item);
+    });
+    
+    console.log(`나의 필통에는 ${this.type}이 들어있습니다.`);
+  }
+}
